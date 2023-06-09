b259e3c24f44a2a47af626781f221ad196ae6eb2
ilkor4 <kornelyuck.ilya@yandex.ru>
2023-06-09 12:08:06 +0300

    feat: Добавить логгирование

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookeyParser = require('cookie-parser');
 const { errors } = require('celebrate');
+
 const { userRouter, movieRouter } = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const NotFoundError = require('./errors/not-found-err');
 const globalErrorsHandler = require('./middlewares/globalErrorsHandler');
 
 const app = express();
@@ -17,8 +21,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
 app.use(express.json());
 app.use(cookeyParser());
 
+app.use(requestLogger);
+
 app.use('/', userRouter);
 app.use('/', movieRouter);
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Ошибка 404. Данного ресурса не существует.'));
+});
+
+app.use(errorLogger);
+
+app.use(errors());
 
 app.use(globalErrorsHandler);
 
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
+const { regex } = require('../utils/constants');
 
 const {
   signup,
@@ -9,7 +11,13 @@ const {
   updateCurrentUser,
 } = require('../controllers/user');
 
-router.post('/signup', signup);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    password: Joi.string().required(),
+    email: Joi.string().required().email(),
+  }),
+}), signup);
 router.post('/signin', signin);
 router.post('/signout', signout);
 
